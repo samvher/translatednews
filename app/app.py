295ade770fecdded3cdf1e625ff35442e1cfbd31
Sam van Herwaarden <samvherwaarden@gmail.com>
2018-01-21 17:53:12 +0000

    Changed indent level of app.py to 2 spaces

--- app/app.py
+++ app/app.py
@@ -27,21 +27,21 @@ roles_users = db.Table('roles_users',
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+  id = db.Column(db.Integer(), primary_key=True)
+  name = db.Column(db.String(80), unique=True)
+  description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(255), unique=True)
+  password = db.Column(db.String(255))
+  active = db.Column(db.Boolean())
+  confirmed_at = db.Column(db.DateTime())
+  roles = db.relationship('Role', secondary=roles_users,
+                          backref=db.backref('users', lazy='dynamic'))
 
-    def __str__(self):
-        return '<User id=%s email=%s>' % (self.id, self.email)
+  def __str__(self):
+    return '<User id=%s email=%s>' % (self.id, self.email)
 
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
@@ -52,7 +52,7 @@ db.create_all()
 # Views
 @app.route('/')
 def home():
-    return render_template('index.html')
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
